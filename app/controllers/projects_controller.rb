50e7db89bad6f4b9c63c4e7cdb8e6c066cb652cb
Your Name <you@example.com>
2015-02-10 17:23:39 -0800

    AJAXIFY delete tasks and create tasksgit add .

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,52 +1,71 @@
 class ProjectsController < ApplicationController
       before_action :authenticate_user!, :only => [:new]
 
+
+
   def index
     @projects = Project.all.paginate(:page => params[:page])
     # @posts = Project.paginate(:page => params[:page])
   end
 
+
+
+
   def new
     @project = Project.new
   end
 
+
+
+
   def create
-
     # render :text =>params[:project]
-     @project = Project.new(params.require(:project).permit(:title, :body))
-     @project.user=current_user
-
-        if  @project.save
-            flash[:notice] = "Project successfully created"
-            redirect_to projects_path
-            # redirect_to @project
-        else
-          render 'new'
-        end
+      @project = Project.new(params.require(:project).permit(:title, :body))
+      @project.user=current_user
+      if  @project.save
+      flash[:notice] = "Project successfully created"
+      redirect_to projects_path
+      # redirect_to @project
+      else
+      render 'new'
+      end
   end
 
-  def show
 
+
+
+
+  def show
     @project = Project.find(params[:id])
     @task = @project.tasks.new
     @tasks= @project.tasks.all
-
   end
+
+
+
 
   def edit
     @project = Project.find(params[:id])
   end
 
+
+
+
   def update
     @project =Project.new(params.require(:project).permit(:title, :body))
     @project.user=current_user
       if @project.save
-        flash[:notice] = "Project changes saved"
-        redirect_to projects_path
+      flash[:notice] = "Project changes saved"
+      redirect_to projects_path
       else
-        render 'edit'
-      end
+      render 'edit'
+π      end
   end
+
+
+
+
+
 
   def destroy
     @project=Project.find(params[:id])
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,45 @@
 class TasksController < ApplicationController
 
-      before_action :authenticate_user!, :only => [:create]
-  def create
-    @project=Project.find(params[:project_id])
-    @task=@project.tasks.new(params.require(:task).permit( [:title, :body] ))
-    if @task.save
-    redirect_to @project, notice: "Task created!"
-    else
-    flash.now[:alert] = "Something went wrong. try again please!"
-    redirect_to @project
-    end
-  end
+  before_action :authenticate_user!, :only => [:create]
+  
 
-  def destroy
-    @project=Project.find(params[:project_id])
-    @task=Task.find(params[:id])
-    if @task.destroy
-        redirect_to project_path, notice:"Task Deleted!"
-    else
-        redirect_to project_path, notice:"There was an error while deleting task, try again!"
+    def create
+      @project=Project.find(params[:project_id])
+      @task=@project.tasks.new(params.require(:task).permit( [:title, :body] ))
+      
+      respond_to do |format|
+        if @task.save
+        format.html{redirect_to @project, notice: "Task created!"}
+        # format.js{render js: "alert('created');"}
+        # format.json{render json: @task}
+         format.js{render}
+        else
+        format.html {
+        flash.now[:alert] = "Something went wrong. try again please!"
+        redirect_to @project }
+        format.js {render js: "alert('ERROR');"}
+        end
+      end
     end
 
-end
+
+    def destroy
+      @project=Project.find(params[:project_id])
+      @task=Task.find(params[:id])
+
+      respond_to do |format|
+        if @task.destroy
+        format.html{redirect_to project_path(@project), notice:"Task Deleted!"}
+          # format.js{render js:"alert('deleted!');"}
+            format.js{render}
+        else
+        format.html{
+        redirect_to project_path, 
+        notice:"There was an error while deleting task, try again!"}
+        format.js {render js:"alert('error');"}
+        end
+      end
+    end
 
 
 end
